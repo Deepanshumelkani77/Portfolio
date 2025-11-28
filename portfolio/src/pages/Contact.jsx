88b10372239721ef5123c4717d1a8dbc91fc98e5
Deepanshumelkani77 <deepumelkani123@gmail.com>
2025-11-28 12:49:45 +0530

    change backend link

--- portfolio/src/pages/Contact.jsx
+++ portfolio/src/pages/Contact.jsx
@@ -31,7 +31,7 @@ const Contact = () => {
     const loadingToast = toast.loading('Sending message...');
 
     try {
-      const response = await fetch('http://localhost:5000/api/contact', {
+      const response = await fetch('https://portfolio-backend-aoqc.onrender.com/api/contact', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
